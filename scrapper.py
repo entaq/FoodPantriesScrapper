cd38d61c6e452d648fe0a485f88f8e5e7e2bd124
Arun Nagarajan <arun.nagarajan@gmail.com>
2016-01-13 21:31:24 -0500

    added error checking and more file outputs

--- scrapper.py
+++ scrapper.py
@@ -10,58 +10,64 @@ class KitchenRecord:
 kitchens_records = []
 
 def printKitchenDetails(kitchen_record):
-    response = requests.get(kitchen_record.url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    address_div = soup.find('div',{'class':'grid_8'})
+    print kitchen_record.url
+    try:
+        response = requests.get(kitchen_record.url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        address_div = soup.find('div',{'class':'grid_8'})
 
-    script_div = soup.find_all('script')[12]
-    m = re.search('data: "(.*?)"', script_div.text)
-    query_string = m.group(1)
+        script_div = soup.find_all('script')[12]
+        m = re.search('data: "(.*?)"', script_div.text)
+        query_string = m.group(1)
 
-    #trickery here as the address details is hidden behind an ajax call that requires a manual cick
-    response = requests.get('http://www.foodpantries.org/address/get_address.php?'+query_string,
-                            headers={'Referer':'http://www.foodpantries.org'})
+        #trickery here as the address details is hidden behind an ajax call that requires a manual cick
+        response = requests.get('http://www.foodpantries.org/address/get_address.php?'+query_string,
+                                headers={'Referer':'http://www.foodpantries.org'})
 
-    mini_soup = BeautifulSoup(response.content,'html.parser')
-    # print kitchen_record.url
-    if isinstance(mini_soup.contents[1],Tag):
-        website_line = mini_soup.contents[1].text.strip()
-    else:
-        website_line = mini_soup.contents[1].strip()
+        mini_soup = BeautifulSoup(response.content,'html.parser')
+        if len(mini_soup.contents) > 1:
+            if isinstance(mini_soup.contents[1],Tag):
+                website_line = mini_soup.contents[1].text.strip()
+            else:
+                website_line = mini_soup.contents[1].strip()
 
-    m = re.search('^Website: (.*?)$', website_line)
-    website = m.group(1)
-    kitchen_record.street_address = mini_soup.contents[4].strip()
-    kitchen_record.city_state = address_div.contents[12].strip()
-    kitchen_record.phone_number = address_div.contents[14].strip()
-    kitchen_record.website = website
-    kitchens_records.append(kitchen_record)
-
+            m = re.search('^Website: (.*?)$', website_line)
+            website = m.group(1)
+            kitchen_record.street_address = mini_soup.contents[4].strip()
+            kitchen_record.city_state = address_div.contents[12].strip()
+            kitchen_record.phone_number = address_div.contents[14].strip()
+            kitchen_record.website = website
+            kitchens_records.append(kitchen_record)
+    except Exception, e:
+        print e
 
 def printKitchensInCity(state_name, state_url, state_count,listing_url,city_name,city_record_count):
-    response = requests.get(listing_url)
-    soup = BeautifulSoup(response.content, "html.parser")
+    try:
+        response = requests.get(listing_url)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    listing_soup = soup.find_all('div', {'class':'grid_8'})[0]
-    listings = listing_soup.find_all('h2')
-    #skip the first two links as they're not for the kitchens
-    #use the count from the city listing as we don't want "nearby"
-    count = int(city_record_count) + 2
-    i = 1
-    for x in range(2,count) :
-        kitchen = listings[x].find('a')
-        kitchen_record = KitchenRecord()
-        kitchen_record.state_name = state_name
-        kitchen_record.state_url = state_url
-        kitchen_record.state_count = state_count
-        kitchen_record.city_name = city_name
-        kitchen_record.city_listing_url = listing_url
-        kitchen_record.city_record_count = city_record_count
-        kitchen_record.number = str(i)
-        kitchen_record.name = kitchen.text
-        kitchen_record.url = kitchen['href']
-        printKitchenDetails(kitchen_record)
-        i = i + 1
+        listing_soup = soup.find_all('div', {'class':'grid_8'})[0]
+        listings = listing_soup.find_all('h2')
+        #skip the first two links as they're not for the kitchens
+        #use the count from the city listing as we don't want "nearby"
+        count = int(city_record_count) + 2
+        i = 1
+        for x in range(2,count) :
+            kitchen = listings[x].find('a')
+            kitchen_record = KitchenRecord()
+            kitchen_record.state_name = state_name
+            kitchen_record.state_url = state_url
+            kitchen_record.state_count = state_count
+            kitchen_record.city_name = city_name
+            kitchen_record.city_listing_url = listing_url
+            kitchen_record.city_record_count = city_record_count
+            kitchen_record.number = str(i)
+            kitchen_record.name = kitchen.text
+            kitchen_record.url = kitchen['href']
+            printKitchenDetails(kitchen_record)
+            i = i + 1
+    except Exception, e:
+        print e
 
 
 response = requests.get('http://www.foodpantries.org/')
@@ -82,13 +88,11 @@ for state in states:
         city_name = m.group(1)
         count = m.group(2)
         printKitchensInCity(state_name, state_url, state_count, listing_url,city_name,count)
-
-
-with open('kitchens.csv', 'wb') as csvfile:
-    headers = ['State','State URL','State count','City','City URL','City Count','Index in City','Kitchen Name','Listing URL','Street Address','City and State','Phone Number','Kitchen Website']
-    kitchen_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-    kitchen_writer.writerow(headers)
-    for k in kitchens_records:
-        row = [k.state_name,k.state_url,k.state_count,k.city_name,k.city_listing_url,k.city_record_count,k.number,k.name,k.url,k.street_address,k.city_state,k.phone_number,k.website]
-        # print row
-        kitchen_writer.writerow([s.encode('utf8') if type(s) is unicode else s for s in row])
+    with open('kitchens_'+state_name.replace(' ','_')+'.csv', 'wb') as csvfile:
+        # headers = ['State','State URL','State count','City','City URL','City Count','Index in City','Kitchen Name','Listing URL','Street Address','City and State','Phone Number','Kitchen Website']
+        kitchen_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        # kitchen_writer.writerow(headers)
+        for k in kitchens_records:
+            row = [k.state_name,k.state_url,k.state_count,k.city_name,k.city_listing_url,k.city_record_count,k.number,k.name,k.url,k.street_address,k.city_state,k.phone_number,k.website]
+            kitchen_writer.writerow([s.encode('utf8') if type(s) is unicode else s for s in row])
+    kitchens_records = []
